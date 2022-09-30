d3ff4dd141a36e27536579721f4fc88f2d6fe8bc
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-09-29 20:07:19 -0400

    Added Swift-Crypto library for hashing support.

--- swift-s3-access-sample/main.swift
+++ swift-s3-access-sample/main.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import Crypto
+
 import SotoCore
 import SotoS3
 
@@ -22,3 +24,12 @@ guard let accessKey = env["STORAGE_KEY"],
 let client = AWSClient(credentialProvider: .static(accessKeyId: accessKey, secretAccessKey: accessSecret), httpClientProvider: .createNew)
 //let s3 = S3(client: client, region: .useast1) // AWS S3
 let s3 = S3(client: client, endpoint: storageEndpoint) // MinIO
+
+let fileUrl = URL(fileURLWithPath: testFile)
+let fileExtension = fileUrl.pathExtension
+
+if let nameHash = testFile.data(using: .utf8) {
+	let sha = SHA256.hash(data: nameHash)
+	let fileName = "\(sha).\(fileExtension)"
+	print("File: \(fileName)")
+}
